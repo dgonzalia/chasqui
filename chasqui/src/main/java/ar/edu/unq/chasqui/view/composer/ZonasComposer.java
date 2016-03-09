335dfbc71ddc0ab298d3eeb2759ea0b94fa40224
matlock <jfflores90@gmail.com>
2016-03-09 13:24:22 -0300

    se agrega mock de como se verá la ventana de alta de zonas

--- chasqui/src/main/java/ar/edu/unq/chasqui/view/composer/ZonasComposer.java
+++ chasqui/src/main/java/ar/edu/unq/chasqui/view/composer/ZonasComposer.java
@@ -1,5 +1,38 @@
 package ar.edu.unq.chasqui.view.composer;
 
-public class ZonasComposer {
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zkplus.databind.AnnotateDataBinder;
+import org.zkoss.zul.Image;
 
+@SuppressWarnings("deprecation")
+public class ZonasComposer extends GenericForwardComposer<Component> {
+
+	
+	private Image imagen;
+	private AnnotateDataBinder binder;
+	
+	public void doAfterCompose(Component c) throws Exception{
+		super.doAfterCompose(c);
+		imagen.setSrc("/imagenes/subirImagen.png");
+		binder = new AnnotateDataBinder(c);
+		binder.loadAll();
+	}
+
+	
+	
+	
+	public Image getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(Image imagen) {
+		this.imagen = imagen;
+	}
+	
+	
+	
+	
+	
+	
 }
