8b120e1384cfc5e746386df5ae8cd69e3ce58f72
matlock <jfflores90@gmail.com>
2016-10-07 19:48:46 -0300

    se quita servicio @OPTIONS

--- chasqui/src/main/java/ar/edu/unq/chasqui/service/rest/impl/UsuarioListener.java
+++ chasqui/src/main/java/ar/edu/unq/chasqui/service/rest/impl/UsuarioListener.java
@@ -88,17 +88,6 @@ public class UsuarioListener {
 		return Response.ok().build();
 	}
 	
-	@OPTIONS
-	@Path("/edit")
-	public Response options() {
-	    return Response.ok("")
-	            .header("Access-Control-Allow-Origin", "*")
-	            .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-	            .header("Access-Control-Allow-Credentials", "true")
-	            .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-	            .header("Access-Control-Max-Age", "1209600")
-	            .build();
-	}
 	
 	
 	
